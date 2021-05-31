53ccc96c487c8de7ba33f8c16971fbfcdb48f59d
unknown <mihir.sharma2001@gmail.com>
2021-05-31 21:44:59 +0530

    update

---
+++ form.php
@@ -0,0 +1,19 @@
+<?php
+    $name = $_POST['email'];
+
+    $email_from = 'mihirsh02@mihirsh02.github.io';
+
+    $email_subject = "New Form";
+
+    $email_body = "Message: $name.\n";
+
+    $to = "mihir.shaarma2001@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+?>
+
